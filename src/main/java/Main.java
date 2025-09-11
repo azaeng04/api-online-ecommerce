e2b0e8e985bb649dab1c584b972c1ea19f2db439
Azariah Engelbrecht <azaeng04@gmail.com>
2025-09-11 18:52:34 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration.class
+})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
